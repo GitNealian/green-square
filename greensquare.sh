264ad6abfaedfc43af36cc9e3bd699cd6ef6d203
nealian <nealian@163.com>
2019-11-28 11:09:37 +0800

    commit 1

---
+++ greensquare.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+repository_dir=~/green-square
+commits_dir=$repository_dir/commits
+count=1
+commit_file=$commits_dir/`date +"%Y-%m-%d"`_"$count".txt
+
+echo $commit_file
+cd $repository_dir
+
+echo "make it green" > $commit_file
+git add .
+git commit -m "commit $count"
+
