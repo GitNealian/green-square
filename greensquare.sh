710ddf70535f9a3bd2545c06ecb392472b45bca1
nealian <nealian@163.com>
2019-11-28 11:13:23 +0800

    commit 11/28/19

--- greensquare.sh
+++ greensquare.sh
@@ -2,13 +2,12 @@
 
 repository_dir=~/green-square
 commits_dir=$repository_dir/commits
-count=1
-commit_file=$commits_dir/`date +"%Y-%m-%d"`_"$count".txt
+commit_file=$commits_dir/`date +"%Y-%m-%d"`.txt
 
 echo $commit_file
 cd $repository_dir
 
 echo "make it green" > $commit_file
 git add .
-git commit -m "commit `date +"%D"`_$count"
-
+git commit -m "commit `date +"%D"`"
+git push
